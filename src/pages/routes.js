8e5fa63bf37a3fb0213315c4caf71bcec378ebd5
iHarryD <anuraagvr555@gmail.com>
2022-05-22 15:57:38 +0530

    resolved conflict

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import ExplorePage from "./explorePage/ExplorePage";
 import PlaylistPage from "./playlistPage/PlaylistPage";
+import SingleVideoPage from "./singleVideoPage/SingleVideoPage";
 
 export default function AllRoutes() {
   return (
     <Routes>
       <Route path="/" element={<ExplorePage />} />
       <Route path="/playlist" element={<PlaylistPage />} />
+      <Route path="/explore" element={<ExplorePage />} />
+      <Route path="/watch/:videoID" element={<SingleVideoPage />} />
+      <Route path="*" element={<ExplorePage />} />
     </Routes>
   );
 }
